fa3704cc0092c6d41954f6c638758a2a89cae4ec
Jonathan van 't Ende <jvantende@onetoweb.nl>
2021-06-29 17:10:44 +0200

    update webhook.php

--- webhook.php
+++ webhook.php
@@ -3,8 +3,6 @@
 require 'vendor/autoload.php';
 
 use Onetoweb\NOWPayments\Authentication;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 $ipnSecret = 'ipn secret';
 
@@ -12,7 +10,7 @@ $ipnSecret = 'ipn secret';
 // https://symfony.com/doc/current/components/http_kernel.html
 // requires symfony http kernel: (composer require symfony/http-kernel)
 
-$request = Request::createFromGlobals();
+$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
 if(Authentication::authenticate($request->getContent(), $request->headers->get('http_x_nowpayments_sig'), $ipnSecret)) {
     
     // webhook authenticated
