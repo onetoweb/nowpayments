eb2480c7a8ce5a6d4e1ee1671c5070265c70ab29
Kh.ab <83933318+mkhab7@users.noreply.github.com>
2022-09-02 01:59:08 +0430

    Update helpers.php
    Add test mode param

--- src/helpers.php
+++ src/helpers.php
@@ -3,8 +3,8 @@
 use Onetoweb\NOWPayments\Client;
 
 if (!function_exists('newPay')) {
-    function newPay(string $apiKey = null): Client
+    function newPay(string $apiKey = null,bool $testMode = false): Client
     {
-        return new Client($apiKey ?? getenv('NEW_PAY_API_KEY'));
+        return new Client($apiKey ?? getenv('NEW_PAY_API_KEY'),$testMode);
     }
 }
